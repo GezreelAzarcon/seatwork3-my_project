b07e44272f198db069f30b8bd59985ebd27f9b5d
GezreelAzarcon <gezreelwazarcon@gmail.com>
2022-11-19 18:26:11 +0800

    added more anime from my watchlist!

--- animelist.py
+++ animelist.py
@@ -1,6 +1,23 @@
-anime = [
-    ['one piece', 'adventure', 'long', 'series'],
-    ['fairy tail', 'fantasy', 'long', 'series'],
-    ['black clover', 'fantasy', 'long', 'series'],
-    ['anohana', 'drama', 'short', ['series', 'movie']]
+anime =[
+    ['One Piece', 'shounen', 'long', 'series'],
+    ['Fairy Tail', 'shounen', 'long', 'series'],
+    ['Black Clover', 'shounen', 'long', 'series'],
+    ['Anohana', 'drama', 'short', 'series'],
+    ["Miss Kobayashi's Dragon Maid", 'slice of life', 'short', 'series'],
+    ['Mob Psycho', 'slice of life', 'short', 'series'],
+    ['Your Name', 'drama', 'short', 'movie'],
+    ['A Silent Voice', 'drama', 'short', 'movie'],
+    ['A Garden of Words', 'drama', 'short', 'movie'],
+    ['Sword Art Online', 'shounen', 'long', 'series'],
+    ['My Hero Academia', 'shounen', 'long', 'series'],
+    ['Chainsaw Man', 'shounen', 'short', 'series'],
+    ['Blue Lock', 'sports', 'short', 'series'],
+    ['86 EIGHTY-SIX', 'shounen', 'short', 'series'],
+    ['Akame ga Kill', 'shounen', 'short', 'series'],
+    ["Akebi's Sailor Uniform", 'slice of  life',  'short', 'series'],
+    ['Aoashi', 'sports', 'short', 'series']
+    ['Assassination Classroom', 'shounen', 'long', 'series'],
+    ['Overlord', 'shounen', 'long', 'series'],
+    ['Wolf Children', 'drama', 'short', 'movie'],
+    ['5 Centimeters Per Second', 'drama', 'short', 'series']
 ]
