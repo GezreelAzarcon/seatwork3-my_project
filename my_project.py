a46dcd08cc575061b2f9b92498345380240b3c1f
GezreelAzarcon <gezreelwazarcon@gmail.com>
2022-11-19 12:43:38 +0800

    my first draft of pseudo code done!

---
+++ my_project.py
@@ -0,0 +1,12 @@
+#RANDOM PICKER FOR ANIMEs TO BINGE
+
+#import random
+#create a list of animes that i want to watch
+#sort the animes based on if it is a movie or a series
+#sort the animes based on its mood
+
+#ask for user input on what type he want to watch (movie or series)
+#if movie get the index of nested movie list else get the series one
+#ask user about their mood
+#get the index of a nested list depending on mood
+#print the ANIME!
